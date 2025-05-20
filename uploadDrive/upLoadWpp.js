324c15b45c725826a009480f8497c08f39826627
Leonardo Silva <leonad96std@gmail.com>
2025-05-19 23:05:04 -0300

    Correção de erro na variavel

--- uploadDrive/upLoadWpp.js
+++ uploadDrive/upLoadWpp.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const upload = require("./googleDriveUploader"); 
+//const upload = require("./googleDriveUploader"); 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
