1228842a92647896ae15f269ae4c3859aa9f20d5
Leonardo Silva <leonad96std@gmail.com>
2025-05-20 23:28:33 -0300

    2º Alteração no Cors

--- uploadDrive/upLoadWpp.js
+++ uploadDrive/upLoadWpp.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:'https://test-tshirt-custom.netlify.app',
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 
 
 const multer = require("multer");
