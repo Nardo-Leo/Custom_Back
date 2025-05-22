5e3e39e4bfd2340f4180929130d587747a0b8035
Leonardo Silva <leonad96std@gmail.com>
2025-05-21 22:56:08 -0300

    verificando a credencial

--- UploadDrive.js
+++ UploadDrive.js
@@ -9,6 +9,8 @@ const Google_Api_Folder_Id = process.env.Google_Api_Folder_Id
 //const Cred_Google = process.env.Cred_Google
 
 const Cred_Google = process.env.Cred_Google
+
+console.log("AQUI ESTÁ ==>>..."+Cred_Google)
  
 const auth = new google.auth.GoogleAuth({
     //keyFile: Cred_Google
